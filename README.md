391adaa54198b020dd5e510560db6a8e42259310
Edward Leung <edward_leung_12@hotmail.com>
2020-08-14 15:02:42 +0100

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# 7_15_project_files
+
+So, I think that this was a good project because it gave you a lot of insight into things that we've looked at such as the Fetch API, Promises, Async/await, ES6 classes, arrow functions and pretty much all the core fundamentals we have gone through so far.
--- githubfinder/github.js
+++ githubfinder/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "22a293972e894d1cf9b8";
     this.client_secret = "75fe1f932ea55318e586099c69074de6c723f02e";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -10,12 +12,21 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    // await response of the fetch call for the REPOS
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     // Only proceed once its resolved
     const profile = await profileResponse.json();
+
+    // Only proceed once its resolved
+    const repos = await repoResponse.json();
 
     // Only proceed once second promise is resolved
     return {
       profile,
+      repos,
     };
   }
 }
